a85d52315aae6bb1426593dcc1c1dd9771acf5c6
Matthew Mayer <grafixgeek@yahoo.com>
2024-01-21 12:26:14 -0500

    New Deploy January 2024

--- js/Catalog.js
+++ js/Catalog.js
@@ -2,7 +2,7 @@ const Book = class {
     constructor(book) {
         this.bookNumber  = book[0];
         this.title       = book[1];
-        this.image       = 'images/puppy_love/'+book[2];
+        this.image       = 'images/books/'+book[2]+'.jpg';
         this.collection  = book[3];
         this.description = book[4];
         this.breed       = book[5]
@@ -12,20 +12,24 @@ const Book = class {
             this.title
         );
     }
-    bookListing() {
-        const bookCard =
-            `<div class="card">
-                <img class="card-img-top" src="${this.image}" alt="${this.breed}">
-                <div class="card-body">
-                    <h4 class="card-title">${this.breed}</h4>
-                    <button class="buy" onclick="window.open('https://www.amazon.com/dp/${this.bookNumber}')">
-                        Buy Now!
-                    </button>
+    bookListing(collection) {
+        if(this.collection === collection){
+            const bookCard =
+                `<div class="card">
+                    <img class="card-img-top" src="${this.image}" alt="${this.breed}">
+                    <div class="card-body">
+                        <h4 class="card-title">${this.breed}</h4>
+                        <button class="buy" onclick="window.open('https://www.amazon.com/dp/${this.bookNumber}')">
+                            Buy Now!
+                        </button>
+                    </div>
                 </div>
-             </div>
-            `   ;
-            return bookCard;
+                `   ;
+                return bookCard;
+        } else {
+            return;
         }
+    }
 };
 
 //let newBook = new Book(book);
